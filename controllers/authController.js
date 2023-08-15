62d5ae23dab7d6923ceac2dfa0ac62eec2f0082d
Masonb18 <bauer.mason01@gmail.com>
2023-08-15 15:35:42 -0500

    controllers

---
+++ controllers/authController.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// Define authentication routes (signup, login, logout)
+// Import User model and implement signup, login, and logout logic
+
+module.exports = router;
